02449659c7bcd73ec427c2dfe130afbc6fa3cfc5
Michael Burns <michael.burns@rackspace.com>
2015-12-27 21:07:30 -0800

    add unit test scaffolding

---
+++ test/unit/spec/default_spec.rb
@@ -0,0 +1,9 @@
+require 'chefspec'
+require_relative 'spec_helper'
+
+describe 'megacli::default' do
+  before { stub_resources }
+
+  let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
+
+end
