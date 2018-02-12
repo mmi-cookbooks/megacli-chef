41d2c6c7dc0ec8e7b322187005441ac55d90b286
Michael Burns <michael@mirwin.net>
2018-02-11 18:11:22 -0800

    update metadata, bump version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,10 @@ maintainer_email 'sfo-devops@lists.rackspace.com'
 license 'Apache 2.0'
 description 'Installs/Configures megacli tools'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.3.0'
+version '0.3.1'
+chef_version '>= 12'
 
 supports 'ubuntu'
+
+source_url 'https://github.com/mmi-cookbooks/megacli-chef' if respond_to?(:source_url)
+issues_url 'https://github.com/mmi-cookbooks/megacli-chef/issues' if respond_to?(:issues_url)
